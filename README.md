c7e4be5f5ccccd6690c29d38744479d324217834
Kelley van Evert <hello@kelleyvanevert.nl>
2020-10-06 17:51:02 +0200

    Fetching and rendering data

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 1. Created add with create-react-app
 2. Added basic supporting packages
 3. Scaffolding basic folder structure and initial pages
+4. Fetching and rendering data
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,15 +1,70 @@
 // src/home/HomePage.tsx
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import { Typography, Container } from "@material-ui/core";
+import {
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  Container,
+} from "@material-ui/core";
+
+import { FetchState } from "../../util/fetchstate";
+import { PostsResponse } from "../../lib/model";
 
 export default function HomePage() {
+  const [state, setState] = useState<FetchState<PostsResponse>>({
+    status: "loading",
+  });
+
+  useEffect(() => {
+    (async () => {
+      setState({ status: "loading" });
+      try {
+        const res = await axios.get(
+          "https://codaisseur-coders-network-okta.herokuapp.com/posts"
+        );
+        setState({ status: "success", data: res.data });
+      } catch (error) {
+        setState({ status: "error", error });
+      }
+    })();
+  }, []);
+
   return (
     <Container fixed>
       <Typography variant="h3" component="h1">
         Codaisseur Coders Network
       </Typography>
-      <p>Welcome!</p>
+      {state.status === "loading" && <p>Loading...</p>}
+      {state.status === "error" && <p>ERROR!</p>}
+      {state.status === "success" && (
+        <Grid container spacing={3}>
+          {state.data.rows.map((post) => {
+            return (
+              <Grid key={post.id} item xs={4}>
+                <Card>
+                  <CardContent
+                    style={{ maxHeight: "15rem", overflow: "hidden" }}
+                  >
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {post.title}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      {post.content}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
     </Container>
   );
 }
