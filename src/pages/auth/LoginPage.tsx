b7ea7ff16868e69c7c3207b2b40af5ebb9067d72
Swen Mulderij <swenmulderij@gmail.com>
2022-08-08 14:35:46 +0200

    Scaffolding done

---
+++ src/pages/auth/LoginPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Typography, Container } from "@mui/material";
+
+export default function HomePage() {
+  return (
+    <Container fixed>
+      <Typography variant="h3" component="h1">
+        Codaisseur Coders Network
+      </Typography>
+      <p>Welcome to login!</p>
+    </Container>
+  );
+}
