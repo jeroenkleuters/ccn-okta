671e6578dccb0e01c5c72cbb309c39acb3cec75a
Swen Mulderij <swenmulderij@gmail.com>
2022-08-08 15:31:07 +0200

    Fixed the app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+test("renders learn react link", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText("Signup");
   expect(linkElement).toBeInTheDocument();
 });
